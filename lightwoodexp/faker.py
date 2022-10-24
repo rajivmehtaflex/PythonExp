26e321bef26d068d2b2ca77271ad745fd288c1a8
Rajiv Mehta <rajiv.mehta@live.in>
2022-10-24 13:58:52 +0000

    Start working on practical example

--- lightwoodexp/faker.py
+++ lightwoodexp/faker.py
@@ -5,7 +5,7 @@ from lightwood.data.encoded_ds import EncodedDs, ConcatedEncodedDs
 from lightwood import dtype
 from lightwood.encoder import BaseEncoder
 from loguru import logger
-
+from transformers import pipeline
 import torch
 import pandas as pd
 # from sklearn.ensemble import RandomForestClassifier
@@ -25,6 +25,7 @@ class Faker(BaseMixer):
         # We could also initialize this in `fit` if some of the parameters depend on the input data, since `fit` is called exactly once
         # self.clf = RandomForestClassifier(max_depth=30)
         logger.add("/home/gitpod/out.log")
+        self.sentiment_pipeline = pipeline("sentiment-analysis") 
         # logger.info("If you're using Python {}, prefer {feature} of course!",sys.version , feature="f-strings")
 
     def fit(self, train_data: EncodedDs, dev_data: EncodedDs) -> None:
@@ -42,20 +43,23 @@ class Faker(BaseMixer):
     def __call__(self, ds: EncodedDs,
                  args: PredictionArguments = PredictionArguments()) -> pd.DataFrame:
         # Turn the data into an sklearn friendly format
-        # X = []
+        X = []
         # for x, _ in ds:
         #     # logger.info(f'while prediction process--> {x.tolist()}')
         #     X.append(x.tolist())
 
         # Yh = self.clf.predict(X)
-        logger.info(f'original data --> {ConcatedEncodedDs([ds]).get_column_original_data("reviewtext")}')
+        for item in ConcatedEncodedDs([ds]).get_column_original_data("reviewtext").tolist():       
+            logger.info(f'original data for prediction--> {item}')
+            X.append((1 if self.sentiment_pipeline([item])[0]['label'] == 'POSITIVE' else 0))            
         # # Lightwood encoders are meant to decode torch tensors, so we have to cast the predictions first
         # decoded_predictions = self.target_encoder.decode(torch.Tensor(Yh))
 
         # Finally, turn the decoded predictions into a dataframe with a single column called `prediction`. This is the standard behaviour all lightwood mixers use
         
         # logger.info(f'decoded prediction --> {decoded_predictions} , {type(decoded_predictions)}')
-        decoded_predictions=[str(i) for i in range(len(ds))]
+        # decoded_predictions=[str(i) for i in range(len(ds))]
+        decoded_predictions=[str(i) for i in X]
         logger.info(f'decoded prediction --> {decoded_predictions} , {type(decoded_predictions)}')
         ydf = pd.DataFrame({'prediction': decoded_predictions})
         return ydf
