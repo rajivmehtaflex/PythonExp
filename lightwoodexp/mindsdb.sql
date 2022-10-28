fb28bf06865572e00ab8000b3b69df0099456485
Rajiv Mehta <rajiv.mehta@live.in>
2022-10-28 15:56:55 +0000

    update in sql

--- lightwoodexp/mindsdb.sql
+++ lightwoodexp/mindsdb.sql
@@ -6,42 +6,37 @@
 SHOW TABLES FROM files;
 
 --- These files can be queried as tables, 
---- You just uploaded review
+--- You just uploaded airline
 
-;
-
-SELECT * FROM files.review LIMIT 100
-
-drop table files.review
-
-DROP PREDICTOR mindsdb.reviewdata
+SELECT t.text,t.sentiment 
+FROM files.airline t 
+LIMIT 100;
 
 CREATE PREDICTOR mindsdb.reviewdata
 FROM files
-    (SELECT * FROM files.review LIMIT 100)
-PREDICT positive
+    (SELECT * FROM files.airline LIMIT 100)
+PREDICT sentiment
 USING
     model.args={"submodels":[
 					{
-						'module': 'faker.Faker',
-						'args': {
-							'stop_after': '$problem_definition.seconds_per_mixer',
-							'dtype_dict': '$dtype_dict',
-							'target': '$target',
-							'target_encoder': '$encoders[self.target]'
-						}
+                        "module": "fdf.FetchDB",
+                        "args": {
+                            "target_encoder": "$encoders[self.target]",
+                            "stop_after": "$problem_definition.seconds_per_mixer"
+                        }                        
 					}
                 ]};
 
-
-SELECT positive
-FROM mindsdb.reviewinfo
-WHERE revewtext='ok let us do it!!!'
-
-
-DESCRIBE PREDICTOR mindsdb.reviewdata.features;
-
-
 SELECT *
 FROM mindsdb.predictors
-WHERE name='home_rentals_model';
+
+DESCRIBE PREDICTOR mindsdb.reviewdata.ensemble;
+
+SELECT sentiment
+FROM mindsdb.reviewdata
+WHERE text='Where are you bloodyful'
+
+SELECT t.text,t.sentiment,m.sentiment
+FROM files.airline t
+JOIN mindsdb.reviewdata AS m
+LIMIT 5;
